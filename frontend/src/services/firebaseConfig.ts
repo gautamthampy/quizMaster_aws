3dfd91521a6f239c5a87eb6bee569eb902eb20b6
Gautam S Thampy <thampygautam@gmail.com>
2025-02-25 14:19:06 -0800

    moved secrets to env file

--- frontend/src/services/firebaseConfig.ts
+++ frontend/src/services/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyARn6_GLhVSV6_GigQZZGSPqrpYjYw2zGw",
-  authDomain: "cmpe272-e0b9f.firebaseapp.com",
-  projectId: "cmpe272-e0b9f",
-  storageBucket: "cmpe272-e0b9f.firebasestorage.app",
-  messagingSenderId: "560202303324",
-  appId: "1:560202303324:web:198bfb6ccf36857d0d6399",
-  measurementId: "G-KQST1WHZVM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
